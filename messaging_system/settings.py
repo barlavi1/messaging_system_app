26fd51c94c4afe17c3e5bf18fc22a1ddd4e7ab8c
Bar Lavi <33284345+barlavi1@users.noreply.github.com>
2024-02-27 22:18:06 +0200

    Update settings.py

--- messaging_system/settings.py
+++ messaging_system/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-kx10(%igl0$x-jxcgmm)3&h$)6(&wyz(uq7zm362op(3#jby-q"
+SECRET_KEY = "add security key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
